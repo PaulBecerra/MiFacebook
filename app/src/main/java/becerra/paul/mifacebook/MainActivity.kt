3bbc9963cb6eac1b7f0362a18ce4fa84753e2b00
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-02-13 16:34:45 -0700

    login activity listener

--- app/src/main/java/becerra/paul/mifacebook/MainActivity.kt
+++ app/src/main/java/becerra/paul/mifacebook/MainActivity.kt
@@ -1,11 +1,21 @@
 package becerra.paul.mifacebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btn_registrar)
+        button.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
