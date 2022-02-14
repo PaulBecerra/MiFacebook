1ac61d90d5d6e4ad8588539b5535ab47c796f251
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-02-13 17:56:52 -0700

    login mockup

--- app/src/main/java/becerra/paul/mifacebook/LoginActivity.kt
+++ app/src/main/java/becerra/paul/mifacebook/LoginActivity.kt
@@ -1,11 +1,18 @@
 package becerra.paul.mifacebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun openMainActivityListener(view: View){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
